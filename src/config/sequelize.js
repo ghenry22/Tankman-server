78ba68d37d4398c6937360d4723a58c12241a162
Gaven Henry <ghenry22@gmail.com>
2024-08-31 14:15:55 +1000

    test some sequelize stuff

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -9,6 +9,18 @@ const sequelize = new Sequelize(
   },
 );
 
+sequelize.afterConnect(async (connection) => {
+  console.log('AFTER CONNECT HOOK');
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
+
 // Sync models with database
 sequelize.sync();
 
